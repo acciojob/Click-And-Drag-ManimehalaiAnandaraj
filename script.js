9065d9579999f25d410f1733313d6140a68cdafc
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-01 13:50:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Your code here.
   const items = document.querySelectorAll('.item');
-//console.log(document.querySelector('.items').scrollLeft)
+console.log(document.querySelector('.items').scrollLeft)
   const container = document.querySelector('.items');
 
 // Loop through each item
